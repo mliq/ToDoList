3f9d89def15e853a2eb3d8671cba963c4ce92394
mliq <mliq@users.noreply.github.com>
2015-01-20 21:13:21 -0600

    delete_spec started

---
+++ spec/features/todo_items/delete_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../spec_helper' # require 'spec_helper'
+
+describe "Deleting todo items" do 
+  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List.") }
+  let!(:todo_item) { todo_list.todo_items.create(content:"Milk") }
+
+  def visit_todo_list(list)
+  visit "/todo_lists"
+  within "#todo_list_#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful when clicking the destroy link" do
+    visit "/todo_lists"
+
+    within "#todo_list_#{todo_list.id}" do
+      click_link "Delete"
+    end
+    
+    expect(page).to_not have_content(todo_list.title)
+    expect(TodoList.count).to eq(0)
+  end
+end
